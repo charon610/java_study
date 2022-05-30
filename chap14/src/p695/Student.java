538cefa4bb77260349e3b766508323658293b778
DESKTOP-J1OP3L4\ez220405 <orca610@naver.com>
2022-05-30 21:42:11 +0900

    0530

---
+++ chap14/src/p695/Student.java
@@ -0,0 +1,29 @@
+package p695;
+
+public class Student {
+	private String name;
+	private int englishScore;
+	private int mathScroe;
+
+	public Student(String name, int englishScore, int mathScroe) {
+		this.name = name;
+		this.englishScore = englishScore;
+		this.mathScroe = mathScroe;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public int getEnglishScore() {
+		return englishScore;
+	}
+
+	public int getMathScroe() {
+		return mathScroe;
+	}
+	
+	
+	
+	
+}
